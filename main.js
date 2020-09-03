c896e66faf68550d9b6105eb437398714d1e5b1e
Chamy619 <38484972+Chamy619@users.noreply.github.com>
2020-09-03 23:46:37 +0900

    Revert "Delete main.js"
    This reverts commit 352f603d183f6b8d188ea1086e6254c63fb653fb.

---
+++ main.js
@@ -0,0 +1,45 @@
+var http = require('http');
+var url = require('url');
+var play = require('./lib/play');
+var Template = require('./lib/Template');
+
+var app = http.createServer(function (request, response) {
+    var _url = request.url;
+    var queryData = url.parse(_url, true);
+    var pathname = url.parse(_url, true).pathname;
+    if (pathname === '/') {
+        play.home(request, response);
+    }
+    else if (pathname === '/income') {
+        play.income(request, response);
+    }
+    else if (pathname === '/income_process') {
+        play.income_process(request, response);
+    }
+    else if (pathname === '/outcome') {
+        play.outcome(request, response);
+    }
+    else if (pathname === '/outcome_process') {
+        play.outcome_process(request, response);
+    }
+    else if (pathname === '/list') {
+        play.list(request, response);
+    }
+    else if (pathname === '/update') {
+        play.update(request, response);
+    }
+    else if (pathname === '/update_process/income') {
+        play.update_income(request, response);
+    }
+    else if (pathname === '/update_process/outcome') {
+        play.update_outcome(request, response);
+    }
+    else if (pathname === '/delete') {
+        play.delete(request, response);
+    }
+    else {
+        response.writeHead(404);
+        response.end('Not Found');
+    }
+});
+app.listen(3000);
