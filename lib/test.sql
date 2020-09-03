9d9a819172b8e1bc4fe3f080c1ea6d011e3e295b
Chamy619 <38484972+Chamy619@users.noreply.github.com>
2020-09-03 23:32:20 +0900

    Delete test.sql

--- lib/test.sql
+++
@@ -1,1 +0,0 @@
-(SELECT id,1 AS type,DATE_FORMAT(calen,'%Y-%m-%d') AS calen, title, price FROM income) UNION (SELECT id,2 AS type,DATE_FORMAT(calen,'%Y-%m-%d') AS calen, title, -price FROM outcome) order by calen;
