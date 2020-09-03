7c6a0a75ae4b351c7b9e6ad4d378814a2a67bb64
Chamy619 <38484972+Chamy619@users.noreply.github.com>
2020-09-03 19:30:13 +0900

    Create woori.sql

--- main.js
+++ main.js
@@ -25,6 +25,9 @@ var app = http.createServer(function (request, response) {
     else if (pathname === '/list') {
         play.list(request, response);
     }
+    else if (pathname === '/update') {
+        play.update(request, response);
+    }
     else {
         response.writeHead(404);
         response.end('Not Found');
